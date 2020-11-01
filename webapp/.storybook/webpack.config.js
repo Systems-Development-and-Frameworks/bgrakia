67c4cee21608222077113ce5dfaa05b98cf25385
nasden <nasdenkov@gmail.com>
2020-11-01 17:17:31 +0100

    added default controls for news list, sass loading for vuetify and controls package

--- webapp/.storybook/webpack.config.js
+++ webapp/.storybook/webpack.config.js
@@ -9,6 +9,8 @@ module.exports = ({config}) => {
       ...config.resolve.alias,
       "@": path.resolve(__dirname, "../src")
   };
+  config.module.rules.push({ test: /\.less$/, loaders: [ 'style-loader', 'css-loader', 'less-loader' ] });
+  config.module.rules.push({ test: /\.styl$/, loader: 'style-loader!css-loader!stylus-loader' });
 
   return config
 }
--- webapp/src/components/news-list/NewsList.stories.js
+++ webapp/src/components/news-list/NewsList.stories.js
@@ -1,6 +1,20 @@
 import NewsList from './NewsList.vue';
+import NewsItem from '../news-item/NewsItem.vue';
+import NewsForm from '../news-form/NewsForm.vue';
+import 'vuetify/dist/vuetify.css';
 
 export default {
     title: 'Components/NewsList',
-    component: NewsList,
-};
+    component: NewsList,  
+}
+
+
+const Template = (args, { argTypes }) => ({
+    props: Object.keys(argTypes),
+    components: { NewsList, NewsItem, NewsForm },
+    template:
+      '<news-list />',
+});
+
+export const Default = Template.bind({});
+//Default.decorators = [(Story) => <v-app><Story/></v-app>]
