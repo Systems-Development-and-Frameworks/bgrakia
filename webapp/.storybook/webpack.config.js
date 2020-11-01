bd79605cf762ee446488fd6829fa5f54fde40660
nasden <nasdenkov@gmail.com>
2020-11-01 15:14:59 +0100

    storybook config added. it now resolves vue components

---
+++ webapp/.storybook/webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+module.exports = ({config}) => {
+/*  config.resolve.modules = [
+    path.resolve(__dirname, ".."),
+    "node_modules",
+  ]*/
+  config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname, "../src")
+  };
+
+  return config
+}
