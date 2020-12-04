3cb603c9994f405bfd597b2e7604c4a4df225f89
Mayk <maik9704@gmail.com>
2020-12-04 16:42:56 +0100

    adds Authorisation check for create

--- backend/src/tests/tests.test.js
+++ backend/src/tests/tests.test.js
@@ -3,30 +3,60 @@ const { ApolloServer, gql } = require("apollo-server");
 const {beforeEach, describe, expect, it} = require("@jest/globals");
 const PostsAPI = require("../datasources/postApi");
 const UsersAPI = require("../datasources/userApi");
+const AuthAPI = require("../datasources/authenticationApi");
 const typeDefs = require('../typeDefs');
 const resolvers = require('../resolvers');
+const permissions = require('../security/permissions');
 const { v4: uuidv4 } = require('uuid');
+const { applyMiddleware } = require('graphql-middleware');
+const { makeExecutableSchema } = require('graphql-tools');
+const jwt = require('jsonwebtoken');
 
 let usersApi = new UsersAPI();
-
 let postsApi = new PostsAPI();
+let authApi = new AuthAPI();
+
+const schema = applyMiddleware(
+    makeExecutableSchema({
+        typeDefs,
+        resolvers,
+    }),
+    permissions,
+);
+
+let reqMock;
+let resMock;
+const context = require('../context');
+const contextPipeline = () => context({ req: reqMock, res: resMock });
 
 const server = new ApolloServer({
-    typeDefs, 
-    resolvers, 
+    schema,
+    context: contextPipeline,
     dataSources: () => {
         return {
           usersApi: usersApi,
-          postsApi: postsApi
+          postsApi: postsApi,
+          authApi: authApi
         }
     }
 });
 
+let mike_auth_token = '';
+let atanas_auth_token = '';
 
 const { query, mutate } = createTestClient(server);
 
-beforeEach(() => {
+beforeEach(async () => {
+    process.env.JWT_SECRET='TEST_SECRET';
+    // For me a mock express.Request containing some headers is enough.
+    // I assume typescript will complain here:
+    reqMock = { headers: {} };
+    resMock = {};
+
     let userIds = [uuidv4(), uuidv4()];
+
+    mike_auth_token = await authApi.createToken(userIds[0]);
+    atanas_auth_token = await authApi.createToken(userIds[1]);
 
     postsApi.posts = [
         { title: "Mike's Post 1", votes: 0, author: userIds[0] },
@@ -127,7 +157,7 @@ describe("query", () => {
   
 });
 
-/*describe("write(post: $postInput)", () => {
+describe("write(post: $postInput)", () => {
 
     let WRITE_POST;
 
@@ -142,33 +172,49 @@ describe("query", () => {
                 }
             }
         `;
-    })
-
-    it("creates new post", async () => {
-        const {
-            data: { write }
-        } = await mutate({ mutation: WRITE_POST, variables: { post: { title: "Title1", author: { name: "Max" } } } });
-
-        expect(write).toEqual({title: "Title1", author: {name: "Max"}});
     });
 
-    it("throws error if author creates post with already existent title", async () => {    
+    it("creates new post - not authenticated", async () => {
+        const {
+            errors:[ error ]
+        } = await mutate({ mutation: WRITE_POST, variables: { post: { title: "Title1"}}});
+
+        expect(error.message).toEqual('Not Authorised!');
+    });
+
+    it("creates new post - authenticated", async () => {
+        reqMock.headers = {authorization: 'Bearer ' + atanas_auth_token};
+
+        const {
+            data: {write}
+        } = await mutate({ mutation: WRITE_POST, variables: { post: { title: "Title1"} } });
+
+        expect(write).toEqual({title: "Title1", author: {name: "Atanas"}});
+    });
+
+    it("throws error if author creates post with already existent title", async () => {
+        reqMock.headers = {authorization: 'Bearer ' + atanas_auth_token};
+
         const {
             errors: [error]
-        } = await mutate({ mutation: WRITE_POST, variables: { post: { title: "The Nothing", author: { name: "Max" } } } });
+        } = await mutate({ mutation: WRITE_POST, variables: { post: { title: "Atanas's Post 1" } } });
 
         expect(error.message).toEqual("Post with this title already exists.");
     });
 
     it("throws error if author doesn't exist", async () => {
+        unvalidAuthToken = await authApi.createToken(uuidv4());
+        reqMock.headers = {authorization: 'Bearer ' + unvalidAuthToken};
+
         const {
             errors: [error]
-        } = await mutate({ mutation: WRITE_POST, variables: { post: { title: "Asdsdasd", author: { name: "blahblah" } } } });
+        } = await mutate({ mutation: WRITE_POST, variables: { post: { title: "Asdsdasd" } } });
 
-        expect(error.message).toEqual("No such author.");
+        expect(error.message).toEqual("Not Authorised!");
     })
 });
 
+/*
 describe("upvote(title: String, voter: UserInput!)", () => {
 
     let UPVOTE_POST;
@@ -187,7 +233,7 @@ describe("upvote(title: String, voter: UserInput!)", () => {
         const {
             data: {upvote}
         } = await mutate({ mutation: UPVOTE_POST, variables: { title: "The Nothing", voter: { name: "Max" } } });
-        
+
         expect(upvote).toMatchObject({ title: "The Nothing", votes: 1 });
     });
 
@@ -195,7 +241,7 @@ describe("upvote(title: String, voter: UserInput!)", () => {
         const {
             errors: [error]
         } = await mutate({ mutation: UPVOTE_POST, variables: { title: "nosuchpost", voter: { name: "Max" } } });
-        
+
         expect(error.message).toEqual("Post with this title doesn't exist");
     });
 
@@ -203,17 +249,18 @@ describe("upvote(title: String, voter: UserInput!)", () => {
         const {
             errors: [error]
         } = await mutate({ mutation: UPVOTE_POST, variables: { title: "The Nothing", voter: { name: "bro" } } });
-        
+
         expect(error.message).toEqual("No such voter.");
-    })
+    });
 
     it("throws error because voter already voted on this post", async () => {
 
         const {
             errors: [error]
         } = await mutate({ mutation: UPVOTE_POST, variables: { title: "The Nothing", voter: { name: "Max" } } });
-        
+
         expect(error.message).toEqual("This voter has already upvoted this article");
     })
-});*/
+});
+*/
 
