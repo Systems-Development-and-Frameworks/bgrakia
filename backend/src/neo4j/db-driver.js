4f5876d4a4faf26ed39d41397be2d01009587aab
Mayk Akifovski <mayk.akifovski@gmail.com>
2021-01-03 13:08:39 +0100

    Update db-driver.js

--- backend/src/neo4j/db-driver.js
+++ backend/src/neo4j/db-driver.js
@@ -1,5 +1,8 @@
 const neo4j = require('neo4j-driver');
 // Transaction retry time is 30 seconds by default, which is reasonable.
-const driver = neo4j.driver('neo4j://localhost:7687');
+const driver = neo4j.driver(
+  "bolt://localhost:7687",
+  neo4j.auth.basic(NEO4J_USERNAME, NEO4J_PASSWORD)
+);
 module.exports = driver;
 
